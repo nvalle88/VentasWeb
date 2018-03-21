0dad5fa024504ebacd1686adb9ef3c67659f35fe
Alex Adriano <aadriano@digitalstrategy.com.ec>
2018-03-21 17:27:09 -0500

    WebVentas

---
+++ WebVentas/WebVentas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebVentas.MvcApplication" Language="C#" %>
---
+++ WebVentas/WebVentas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebVentas.Startup))]
+namespace WebVentas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
