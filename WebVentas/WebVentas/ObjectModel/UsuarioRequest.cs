7008b53ce4801259c1822a6d18913edc7c0440f8
Alex Adriano <aadriano@digitalstrategy.com.ec>
2018-03-22 15:17:15 -0500

    Create Supervisor

---
+++ WebVentas/WebVentas/ObjectModel/UsuarioRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebVentas.ObjectModel
+{
+    public class UsuarioRequest
+    {
+        public int IdUsuario { get; set; }
+
+        public string Foto { get; set; }
+
+        public bool? Estado { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Identificacion { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Telefono { get; set; }
+
+        public int? IdEmpresa { get; set; }
+    }
+}
