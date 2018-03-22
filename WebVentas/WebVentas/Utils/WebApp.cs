426f06ce73f665f1234448f9c68725c6d0b2e3e8
josue <jcatota@digitalstrategy.com.ec>
2018-03-22 13:02:36 -0500

    modificado utils, creacion de carpeta para los modelos

---
+++ WebVentas/WebVentas/Utils/WebApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebVentas.Utils
+{
+   public static class WebApp
+    {
+        public static string BaseAddress { get; set; }
+
+        public static string BaseAddressRM { get; set; }
+
+        public static string BaseAddressSeguridad { get; set; }
+
+        public static string BaseAddressWebAppLogin { get; set; }
+
+        public static string NombreAplicacion { get; set; }
+
+    }
+}
