66eb74930fa41695b0ca9bb82543186ffa849c8e
Néstor Valle <nestorvalle880416@gmail.com>
2018-03-22 11:50:36 -0500

    Login y se agrega la clase Response en Utils y la clase ApiServicio para el consumo de los servicios Rest en la Carpeta Services

--- WebVentas/WebVentas/App_Start/RouteConfig.cs
+++ WebVentas/WebVentas/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartAdminMvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,12 +13,13 @@ namespace WebVentas
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.LowercaseUrls = true;
+            routes.MapRoute("Default", "{controller}/{action}/{id}", new
+            {
+                controller = "Home",
+                action = "Index",
+                id = UrlParameter.Optional
+            }).RouteHandler = new DashRouteHandler();
         }
     }
 }
