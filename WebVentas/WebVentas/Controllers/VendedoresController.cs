70e9667b7858d9b9236e3b237349b1b46b055a26
josue <jcatota@digitalstrategy.com.ec>
2018-03-29 17:15:12 -0500

    borradada carpeta principals y calendarios, agregados archivos a vendedor para mantener un solo proceso, métodos agregados a vendedores controller

--- WebVentas/WebVentas/Controllers/VendedoresController.cs
+++ WebVentas/WebVentas/Controllers/VendedoresController.cs
@@ -471,5 +471,350 @@ namespace WebVentas.Controllers
         }
 
 
+        public async Task<ActionResult> CalendarioIndex(string mensaje)
+        {
+            InicializarMensaje(mensaje);
+
+            var lista = new List<VendedorRequest>();
+            VendedorRequest vendedorRequest = new VendedorRequest();
+            SupervisorRequest supervisorRequest = new SupervisorRequest();
+            int idEmpresaInt = 0;
+
+            var listaEventos = new List<EventoRequest>();
+
+
+            try
+            {
+                var userWithClaims = (ClaimsPrincipal)User;
+                var idEmpresa = userWithClaims.Claims.First(c => c.Type == Constantes.Empresa).Value;
+
+                idEmpresaInt = Convert.ToInt32(idEmpresa);
+
+                vendedorRequest.idEmpresa = idEmpresaInt;
+            }
+            catch (Exception ex)
+            {
+
+                InicializarMensaje(Mensaje.ErrorIdEmpresa);
+                return View(listaEventos);
+            }
+
+
+
+            try
+            {
+                ApplicationDbContext db = new ApplicationDbContext();
+
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+
+                var idUsuarioActual = User.Identity.GetUserId();
+
+                supervisorRequest.IdUsuario = idUsuarioActual;
+                supervisorRequest.IdEmpresa = idEmpresaInt;
+
+                if (userManager.IsInRole(idUsuarioActual, "Supervisor"))
+                {
+                    Response response = await ApiServicio.InsertarAsync(supervisorRequest,
+                                                                 new Uri(WebApp.BaseAddress),
+                                                                 "api/Vendedores/obtenerSupervisorPorIdUsuario");
+
+                    supervisorRequest = JsonConvert.DeserializeObject<SupervisorRequest>(response.Resultado.ToString());
+                    vendedorRequest.IdSupervisor = supervisorRequest.IdSupervisor;
+
+
+                }
+
+
+                lista = await ApiServicio.ObtenerElementoAsync1<List<VendedorRequest>>(vendedorRequest, new Uri(WebApp.BaseAddress)
+                                                              , "api/Vendedores/ListarVendedoresPorSupervisor");
+
+                ViewBag.IdVendedor = new SelectList(lista, "IdVendedor", "Nombres");
+
+
+                return View(listaEventos);
+            }
+            catch (Exception ex)
+            {
+                InicializarMensaje(Mensaje.Excepcion);
+                return View(listaEventos);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CalendarioIndex(int idVendedor)
+        {
+            SupervisorRequest supervisorRequest = new SupervisorRequest();
+            VendedorRequest vendedorRequest = new VendedorRequest();
+
+            var lista = new List<VendedorRequest>();
+            int idEmpresaInt = 0;
+
+
+            try
+            {
+                var userWithClaims = (ClaimsPrincipal)User;
+                var idEmpresa = userWithClaims.Claims.First(c => c.Type == Constantes.Empresa).Value;
+
+                idEmpresaInt = Convert.ToInt32(idEmpresa);
+
+                vendedorRequest.idEmpresa = idEmpresaInt;
+                vendedorRequest.IdVendedor = idVendedor;
+            }
+            catch (Exception ex)
+            {
+
+                InicializarMensaje(Mensaje.ErrorIdEmpresa);
+                return View(lista);
+            }
+
+
+
+            try
+            {
+                // Obtener supervisor
+                ApplicationDbContext db = new ApplicationDbContext();
+
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+
+                var idUsuarioActual = User.Identity.GetUserId();
+
+                supervisorRequest.IdUsuario = idUsuarioActual;
+                supervisorRequest.IdEmpresa = idEmpresaInt;
+
+                if (userManager.IsInRole(idUsuarioActual, "Supervisor"))
+                {
+                    Response response = await ApiServicio.InsertarAsync(supervisorRequest,
+                                                                 new Uri(WebApp.BaseAddress),
+                                                                 "api/Vendedores/obtenerSupervisorPorIdUsuario");
+
+                    supervisorRequest = JsonConvert.DeserializeObject<SupervisorRequest>(response.Resultado.ToString());
+                    vendedorRequest.IdSupervisor = supervisorRequest.IdSupervisor;
+
+
+                }
+
+
+
+                // Carga de combo
+
+                lista = await ApiServicio.ObtenerElementoAsync1<List<VendedorRequest>>(vendedorRequest, new Uri(WebApp.BaseAddress)
+                                                              , "api/Vendedores/ListarVendedoresPorSupervisor");
+
+                ViewBag.IdVendedor = new SelectList(lista, "IdVendedor", "Nombres");
+
+
+
+                // Lista de eventos para cargar la agenda
+                var listaEventos = await ApiServicio.ObtenerElementoAsync1<List<EventoRequest>>(vendedorRequest, new Uri(WebApp.BaseAddress)
+                                                              , "api/Agendas/ListarEventosPorVendedor");
+
+
+                return View(listaEventos);
+            }
+            catch (Exception ex)
+            {
+                InicializarMensaje(Mensaje.Excepcion);
+                return View();
+            }
+        }
+
+
+
+        /*
+         ************************************* Métodos para mapa Y Ajax-mapa
+         */
+
+
+        public async Task<ActionResult> MapaIndex(string mensaje)
+        {
+
+            InicializarMensaje(mensaje);
+
+            return View();
+
+        }
+
+        public async Task<JsonResult> ListaVendedores()
+        {
+
+            var lista = new List<UbicacionPersonaRequest>();
+
+            SupervisorRequest supervisorRequest = new SupervisorRequest();
+            VendedorRequest vendedorRequest = new VendedorRequest();
+
+            int idEmpresaInt = 0;
+
+            try
+            {
+
+                ApplicationDbContext db = new ApplicationDbContext();
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+                // obtener el idEmpresa
+                var userWithClaims = (ClaimsPrincipal)User;
+                var idEmpresa = userWithClaims.Claims.First(c => c.Type == Constantes.Empresa).Value;
+
+                // convertir el idEmpresa a int
+                idEmpresaInt = Convert.ToInt32(idEmpresa);
+
+
+                //** agregar el idEmpresa al vendedorRequest **
+                vendedorRequest.idEmpresa = idEmpresaInt;
+
+
+                var idUsuarioActual = User.Identity.GetUserId();
+
+                supervisorRequest.IdUsuario = idUsuarioActual;
+                supervisorRequest.IdEmpresa = idEmpresaInt;
+
+
+                if (userManager.IsInRole(idUsuarioActual, "Supervisor"))
+                {
+
+                    // obtener el Id del supervisor
+                    Response response = await ApiServicio.InsertarAsync(supervisorRequest,
+                                                                 new Uri(WebApp.BaseAddress),
+                                                                 "api/Vendedores/obtenerSupervisorPorIdUsuario");
+
+                    supervisorRequest = JsonConvert.DeserializeObject<SupervisorRequest>(response.Resultado.ToString());
+
+
+                    //** agregar el id del supervisor al vendedorRequest **
+                    vendedorRequest.IdSupervisor = supervisorRequest.IdSupervisor;
+                }
+
+
+                lista = await ApiServicio.ObtenerElementoAsync1<List<UbicacionPersonaRequest>>(vendedorRequest,
+                                                             new Uri(WebApp.BaseAddress),
+                                                             "api/Vendedores/ListarVendedoresConUbicacionPorSupervisor");
+
+
+                return Json(lista);
+
+            }
+            catch (Exception ex)
+            {
+
+                return Json(false);
+
+            }
+
+        }
+
+        public async Task<JsonResult> ListaClientes()
+        {
+            var lista = new List<ClienteRequest>();
+
+            SupervisorRequest supervisorRequest = new SupervisorRequest();
+            VendedorRequest vendedorRequest = new VendedorRequest();
+
+            int idEmpresaInt = 0; ;
+
+            try
+            {
+                ApplicationDbContext db = new ApplicationDbContext();
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+                // obtener el idEmpresa
+                var userWithClaims = (ClaimsPrincipal)User;
+                var idEmpresa = userWithClaims.Claims.First(c => c.Type == Constantes.Empresa).Value;
+
+                // convertir el idEmpresa a int
+                idEmpresaInt = Convert.ToInt32(idEmpresa);
+
+
+                //** agregar el idEmpresa al vendedorRequest **
+                vendedorRequest.idEmpresa = idEmpresaInt;
+
+
+                var idUsuarioActual = User.Identity.GetUserId();
+
+                supervisorRequest.IdUsuario = idUsuarioActual;
+                supervisorRequest.IdEmpresa = idEmpresaInt;
+
+
+                if (userManager.IsInRole(idUsuarioActual, "Supervisor"))
+                {
+
+                    // obtener el Id del supervisor
+                    Response response = await ApiServicio.InsertarAsync(supervisorRequest,
+                                                                 new Uri(WebApp.BaseAddress),
+                                                                 "api/Vendedores/obtenerSupervisorPorIdUsuario");
+
+                    supervisorRequest = JsonConvert.DeserializeObject<SupervisorRequest>(response.Resultado.ToString());
+
+
+                    //** agregar el id del supervisor al vendedorRequest **
+                    vendedorRequest.IdSupervisor = supervisorRequest.IdSupervisor;
+                }
+
+
+                lista = await ApiServicio.ObtenerElementoAsync1<List<ClienteRequest>>(vendedorRequest,
+                                                             new Uri(WebApp.BaseAddress),
+                                                             "api/Vendedores/ListarClientesPorSupervisor");
+
+
+                return Json(lista);
+
+            }
+            catch (Exception ex)
+            {
+
+                return Json(false);
+
+            }
+
+        }
+
+
+
+        public async Task<JsonResult> ListaRutas(int IdVendedor)
+        {
+            var lista = new List<RutaRequest>();
+
+            VendedorRequest vendedorRequest = new VendedorRequest();
+
+            int idEmpresaInt = 0;
+
+            try
+            {
+                ApplicationDbContext db = new ApplicationDbContext();
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+                // obtener el idEmpresa
+                var userWithClaims = (ClaimsPrincipal)User;
+                var idEmpresa = userWithClaims.Claims.First(c => c.Type == Constantes.Empresa).Value;
+
+                // convertir el idEmpresa a int
+                idEmpresaInt = Convert.ToInt32(idEmpresa);
+
+
+                //** agregar el idEmpresa al vendedorRequest **
+                vendedorRequest.idEmpresa = idEmpresaInt;
+
+                //** agregar el idVendedor al vendedorRequest **
+                vendedorRequest.IdVendedor = IdVendedor;
+
+
+
+                lista = await ApiServicio.ObtenerElementoAsync1<List<RutaRequest>>(vendedorRequest,
+                                                             new Uri(WebApp.BaseAddress),
+                                                             "api/Vendedores/ListarRutaVendedores");
+
+
+                return Json(lista);
+
+            }
+            catch (Exception ex)
+            {
+
+                return Json(false);
+
+            }
+
+        }
+
     }
 }
