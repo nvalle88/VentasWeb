90d97073b2266243afa36830f816122886f02047
Néstor Valle <nestorvalle880416@gmail.com>
2018-04-01 02:13:51 -0500

    funcionalidad para insertar y editar cliente subiendo el logo

---
+++ WebVentas/WebVentas/ObjectModel/ArchivoRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebVentas.ObjectModel
+{
+    public class ArchivoRequest
+    {
+        public string Id { get; set; }
+        public int Tipo { get; set; }
+        public byte[] Array { get; set; }
+    }
+}
