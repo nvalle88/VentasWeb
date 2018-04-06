35dda8ab7c19b6e49140312ff6c77d604d0f3577
Alex Adriano <aadriano@digitalstrategy.com.ec>
2018-04-06 10:57:52 -0500

    Cambios finales Supervisor

--- WebVentas/WebVentas/Controllers/SupervisorController.cs
+++ WebVentas/WebVentas/Controllers/SupervisorController.cs
@@ -320,13 +320,13 @@ namespace WebVentas.Controllers
                     IdEmpresa = empresaActual.IdEmpresa
                 };
                 var result = await userManager.CreateAsync(user, "A12345.1a");
-                //var result = await userManager.CreateAsync(user, supervisorRequest.Identificacion);
                 db.SaveChanges();
                 var recuperarContrasenaRequest = new RecuperarContrasenaRequest();
                 recuperarContrasenaRequest.Email = user.Email;
                 recuperarContrasenaRequest.Codigo = "A12345.1a";
                 var response2 = await ApiServicio.ObtenerElementoAsync1<Response>(recuperarContrasenaRequest, new Uri(WebApp.BaseAddress)
                                                , "api/Usuarios/EnviarContrasena");
+                //varifica el rol
                 userManager.AddToRoles(user.Id, "Supervisor");
                 if (result != null)
                 {
